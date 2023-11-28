36c5a25cc0abb11fae35ed08ce504be82e84e081
Arrow A <141832297+Arrowaaa@users.noreply.github.com>
2023-11-28 12:08:06 -0300

    Aula 28/11/23

---
+++ Qiz/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Qiz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.txbAlternativaA = new System.Windows.Forms.ComboBox();
+            this.txbAlternativaB = new System.Windows.Forms.ComboBox();
+            this.txbAlternativaC = new System.Windows.Forms.ComboBox();
+            this.txbAlternativaD = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btncadastrar = new System.Windows.Forms.Button();
+            this.rtxpergunta = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 236);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(17, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "A)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 264);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "B)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 297);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "C)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "D)";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(342, 236);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(342, 267);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(85, 17);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(342, 301);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(85, 17);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(342, 336);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(85, 17);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // txbAlternativaA
+            // 
+            this.txbAlternativaA.FormattingEnabled = true;
+            this.txbAlternativaA.Location = new System.Drawing.Point(93, 236);
+            this.txbAlternativaA.Name = "txbAlternativaA";
+            this.txbAlternativaA.Size = new System.Drawing.Size(217, 21);
+            this.txbAlternativaA.TabIndex = 8;
+            // 
+            // txbAlternativaB
+            // 
+            this.txbAlternativaB.FormattingEnabled = true;
+            this.txbAlternativaB.Location = new System.Drawing.Point(93, 263);
+            this.txbAlternativaB.Name = "txbAlternativaB";
+            this.txbAlternativaB.Size = new System.Drawing.Size(217, 21);
+            this.txbAlternativaB.TabIndex = 9;
+            // 
+            // txbAlternativaC
+            // 
+            this.txbAlternativaC.FormattingEnabled = true;
+            this.txbAlternativaC.Location = new System.Drawing.Point(93, 297);
+            this.txbAlternativaC.Name = "txbAlternativaC";
+            this.txbAlternativaC.Size = new System.Drawing.Size(217, 21);
+            this.txbAlternativaC.TabIndex = 10;
+            // 
+            // txbAlternativaD
+            // 
+            this.txbAlternativaD.FormattingEnabled = true;
+            this.txbAlternativaD.Location = new System.Drawing.Point(93, 332);
+            this.txbAlternativaD.Name = "txbAlternativaD";
+            this.txbAlternativaD.Size = new System.Drawing.Size(217, 21);
+            this.txbAlternativaD.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(188, -3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 31);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "QUIZ";
+            // 
+            // btncadastrar
+            // 
+            this.btncadastrar.Location = new System.Drawing.Point(146, 371);
+            this.btncadastrar.Name = "btncadastrar";
+            this.btncadastrar.Size = new System.Drawing.Size(102, 49);
+            this.btncadastrar.TabIndex = 13;
+            this.btncadastrar.Text = "Cadastrar";
+            this.btncadastrar.UseVisualStyleBackColor = true;
+            this.btncadastrar.Click += new System.EventHandler(this.btncadastrar_Click);
+            // 
+            // rtxpergunta
+            // 
+            this.rtxpergunta.Location = new System.Drawing.Point(3, 31);
+            this.rtxpergunta.Name = "rtxpergunta";
+            this.rtxpergunta.Size = new System.Drawing.Size(452, 188);
+            this.rtxpergunta.TabIndex = 14;
+            this.rtxpergunta.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 450);
+            this.Controls.Add(this.rtxpergunta);
+            this.Controls.Add(this.btncadastrar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txbAlternativaD);
+            this.Controls.Add(this.txbAlternativaC);
+            this.Controls.Add(this.txbAlternativaB);
+            this.Controls.Add(this.txbAlternativaA);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.ComboBox txbAlternativaA;
+        private System.Windows.Forms.ComboBox txbAlternativaB;
+        private System.Windows.Forms.ComboBox txbAlternativaC;
+        private System.Windows.Forms.ComboBox txbAlternativaD;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btncadastrar;
+        private System.Windows.Forms.RichTextBox rtxpergunta;
+    }
+}
+
---
+++ Qiz/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Qiz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string conexaoString = "server=localhost;user=root;password=;database=quiz;";
+
+        private void btncadastrar_Click(object sender, EventArgs e)
+        {
+            string pergunta = " ";
+            string alternativaA = "", alternativaB = "",
+                alternativaC = "", alternativaD = "";
+            long ultimoID = 0;
+
+            pergunta = rtxpergunta.Text;
+
+            alternativaA = txbAlternativaA.Text;
+            alternativaB = txbAlternativaB.Text;
+            alternativaC = txbAlternativaC.Text;
+            alternativaD = txbAlternativaD.Text;
+
+
+            using (MySqlConnection conexao = new MySqlConnection(conexaoString))
+            {
+                conexao.Open();
+               string scripInsert = "INSERT INTO perguntas(pergunta,Alternativa) VALUE (@questao,@alternativa)";
+
+
+                using (MySqlCommand comando = new MySqlCommand(scripInsert, conexao))
+                {
+                    //substitui os parametros para os valores reais.
+                    comando.Parameters.AddWithValue("@questao", pergunta);
+                    comando.Parameters.AddWithValue("@questao", alternativaA);
+
+                    comando.ExecuteNonQuery();
+
+                    ultimoID = comando.LastInsertedId;
+                    
+
+
+                }
+
+            }
+
+
+            MessageBox.Show("Pergunta Cadastrada com Sucesso!!");
+
+
+        }
+
+
+
+    }
+}
